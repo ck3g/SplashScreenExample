204ffcfaee45ae152e000eaa616d09a82ffee8fd
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2019-01-13 17:23:08 +0100

    Render a Splash Screen

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+class SplashScreen extends React.Component {
+  render() {
+    const viewStyles = [styles.container, { backgroundColor: 'orange' }];
+    const textStyles = {
+      color: 'white',
+      fontSize: 40,
+      fontWeight: 'bold'
+    };
+
+    return (
+      <View style={viewStyles}>
+        <Text style={textStyles}>
+          Splash Screen
+        </Text>
+      </View>
+    );
+  }
+}
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isLoading: true }
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return <SplashScreen />;
+    }
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>Reload the App to see a splash screen</Text>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          Reload the App to see a splash screen
+        </Text>
       </View>
     );
   }
