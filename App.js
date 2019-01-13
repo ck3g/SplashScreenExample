65cc3529a5d57281ae4167d8f8ef457834c44fe3
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2019-01-13 17:34:58 +0100

    Remove Splash Screen when the data get loaded

--- App.js
+++ App.js
@@ -27,6 +27,25 @@ export default class App extends React.Component {
     this.state = { isLoading: true }
   }
 
+  performTimeConsumingTask = async() => {
+    return new Promise((resolve) =>
+      setTimeout(
+        () => { resolve('result') },
+        2000
+      )
+    );
+  }
+
+  async componentDidMount() {
+    // Preload data from an external API
+    // Preload data using AsyncStorage
+    const data = await this.performTimeConsumingTask();
+
+    if (data !== null) {
+      this.setState({ isLoading: false });
+    }
+  }
+
   render() {
     if (this.state.isLoading) {
       return <SplashScreen />;
